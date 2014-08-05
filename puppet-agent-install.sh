a96fe74a69b4f1b7b6862b7d4d4f981ee760c9a8
root <root@osc3.onestopconfigure.net>
2014-08-05 14:52:55 -0400

    Updated the script name to puppet-agent-install.sh

---
+++ puppet-agent-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# Usage: curl https://raw.githubusercontent.com/bijaybisht/puppet/master/puppet-agent-install.sh | bash
+#
+
+install() {
+    rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+    yum install -y puppet
+    yum install -y wget
+    yum install -y git
+    rm /etc/puppet/puppet.conf
+    wget https://raw.githubusercontent.com/bijaybisht/puppet/master/puppet.conf -O /etc/puppet/puppet.conf
+    echo "puppet agent --no-daemonize --verbose --certname $(hostname)"
+}
+
+test -f /etc/redhat-release && install
